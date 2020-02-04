27c741f409a10ab61013c38740f01cf565cd4cc8
Jose Alves Durand Neto <durand.neto@gmail.com>
2020-02-04 13:13:47 -0800

    styling VHInput

--- packages/Components/Input/Simple/styles.js
+++ packages/Components/Input/Simple/styles.js
@@ -2,19 +2,26 @@ import styled from "styled-components";
 import Color from "../../../Colors";
 
 export const Input = styled.input`
-  border: 1px solid ${Color.black.A20};
-  border-radius: 3px;
-  color: ${Color.black.A40};
-  font-family: Roboto;
-  font-size: 0.875rem;
-  line-height: 18px;
-  padding: 0 8px;
-  width: 180px;
+  border: 1px solid #e0e0e0;
+  border-radius: 6px;
+  color: #333333;
+  font-size: 14px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.53;
+  letter-spacing: normal;
+  padding: 9px 24px;
+  width: 100%;
   background: transparent;
 
   ::placeholder {
-    opacity: 0.5;
-    font-size: 0.875rem;
-    color: ${Color.black.A20}
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.53;
+    letter-spacing: normal;
+    color: #9f9f9f;
   }
 `;
