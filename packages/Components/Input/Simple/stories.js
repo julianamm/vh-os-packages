0d08d4b9fbdb0d43fb2512535155b739b8aa4a1e
Jose Alves Durand Neto <durand.neto@gmail.com>
2020-02-04 13:13:47 -0800

    styling VHInput

--- packages/Components/Input/Simple/stories.js
+++ packages/Components/Input/Simple/stories.js
@@ -4,7 +4,9 @@ import VHInput from ".";
 
 storiesOf("Components|Input/Simple", module)
   .add("default", () => (
-    <VHInput placeholder="VH Simple Input" onEvent={e => {
+    <div style={{width: '80%', marginLeft:'1%', padding: '9px'}}>
+      <VHInput placeholder="VH Simple Input" onEvent={e => {
       console.log(e)
     }} />
+    </div>
   ));
