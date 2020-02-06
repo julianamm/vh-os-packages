ad4c74266a79767753d8fb199ca75f99cef422c6
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2020-02-06 16:41:33 -0300

    Create a component with Title and Description

--- packages/Components/Button/styles.js
+++ packages/Components/Button/styles.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import Color from "../../Colors";
 
 const Button = styled.button`
-  background-color: ${Color.primary.main};
-  border: 1px solid ${Color.primary.main};
-  color: ${Color.white.main};
+  background-color: ${Color["gradient-primary"]};
+  border: 1px solid ${Color["gradient-primary"]};
+  color: ${Color["gray-20"]};
   border-radius: 4px;
   font-family: Roboto;
   font-size: 14px;
@@ -18,11 +18,11 @@ const Button = styled.button`
   }
 
   :hover {
-    background-color: ${Color.primary.light};
+    background-color: ${Color["primary-light"]};
   }
 
   :active {
-    background-color: ${Color.primary.dark};
+    background-color: ${Color["primary-dark"]};
   }
 
   ${({ sm }) =>
@@ -58,60 +58,60 @@ const Button = styled.button`
   ${({ primary }) =>
     primary &&
     `
-    background-color: ${Color.primary.main};
-    border: 1px solid ${Color.primary.main};
+    background-color: ${Color["gradient-primary"]};
+    border: 1px solid ${Color["gradient-primary"]};
     :hover {
-      background-color: ${Color.primary.light};
-      border: 1px solid ${Color.primary.light};
+      background-color: ${Color["primary-light"]};
+      border: 1px solid ${Color["primary-light"]};
     }
     :active {
-      background-color: ${Color.primary.dark};
-      border: 1px solid ${Color.primary.dark};
+      background-color: ${Color["primary-dark"]};
+      border: 1px solid ${Color["primary-dark"]};
     }
   `}
 
   ${({ secondary }) =>
     secondary &&
     `
-    background-color: ${Color.secondary.main};
-    border: 1px solid ${Color.secondary.main};
+    background-color: ${Color.secondary};
+    border: 1px solid ${Color.secondary};
     :hover {
-      border: 1px solid ${Color.secondary.light};
-      background-color: ${Color.secondary.light};
+      border: 1px solid ${Color["secondary-light"]};
+      background-color: ${Color["secondary-light"]};
     }
 
     :active {
-      background-color: ${Color.secondary.dark};
-      border: 1px solid ${Color.secondary.dark};
+      background-color: ${Color["secondary-dark"]};
+      border: 1px solid ${Color["secondary-dark"]};
     }
   `}
 
   ${({ danger }) =>
     danger &&
     `
-    background-color: ${Color.danger.main};
-    border: 1px solid ${Color.danger.main};
+    background-color: ${Color.red};
+    border: 1px solid ${Color.red};
     :hover {
-      background-color: ${Color.danger.light};
-      border: 1px solid ${Color.danger.light};
+      background-color: ${Color.red};
+      border: 1px solid ${Color.red};
     }
 
     :active {
-      background-color: ${Color.danger.dark};
-      border: 1px solid ${Color.danger.dark};
+      background-color: ${Color["red-dark"]};
+      border: 1px solid ${Color["red-dark"]};
     }
   `}
 
   ${({ disabled }) =>
     disabled &&
     `
-    color: ${Color.black.A30};
-    background-color: ${Color.black.A5};
-    border: solid 1px ${Color.black.A5};
+    color: ${Color["black-40"]};
+    background-color: ${Color["black-10"]};
+    border: solid 1px ${Color["black-10"]};
     :hover {
-      background-color: ${Color.black.A5};
-      border: 1px solid ${Color.black.A5};
-      color: ${Color.black.A30};
+      background-color: ${Color["black-10"]};
+      border: 1px solid ${Color["black-10"]};
+      color: ${Color["black-40"]};
       cursor: not-allowed;
     }
   `}
@@ -119,8 +119,8 @@ const Button = styled.button`
 `;
 
 export const OutlinedButton = styled.button`
-  color: ${Color.primary.main};
-  border: 1px solid ${Color.primary.main};
+  color: ${Color["gradient-primary"]};
+  border: 1px solid ${Color["gradient-primary"]};
   border-radius: 4px;
   font-family: Roboto;
   font-size: 14px;
@@ -131,15 +131,15 @@ export const OutlinedButton = styled.button`
   background: transparent;
 
   :hover {
-    background-color: ${Color.primary.light};
-    border: 1px solid ${Color.primary.light};
-    color: ${Color.white.light};
+    background-color: ${Color["primary-light"]};
+    border: 1px solid ${Color["primary-light"]};
+    color: ${Color["white-light"]};
   }
 
   :active {
-    background-color: ${Color.primary.dark};
-    border: 1px solid ${Color.primary.dark};
-    color: ${Color.white.light};
+    background-color: ${Color["primary-dark"]};
+    border: 1px solid ${Color["primary-dark"]};
+    color: ${Color["white-light"]};
   }
 
   ${({ full }) =>
@@ -151,58 +151,58 @@ export const OutlinedButton = styled.button`
   ${({ primary }) =>
     primary &&
     `
-    color: ${Color.primary.main};
-    border: 1px solid ${Color.primary.main};
+    color: ${Color["gradient-primary"]};
+    border: 1px solid ${Color["gradient-primary"]};
     :hover {
-      background-color: ${Color.primary.light};
-      border: 1px solid ${Color.primary.light};
+      background-color: ${Color["primary-light"]};
+      border: 1px solid ${Color["primary-light"]};
     }
     :active {
-      background-color: ${Color.primary.dark};
-      border: 1px solid ${Color.primary.dark};
+      background-color: ${Color["primary-dark"]};
+      border: 1px solid ${Color["primary-dark"]};
     }
   `}
 
   ${({ secondary }) =>
     secondary &&
     `
-    color: ${Color.secondary.main};
-    border: 1px solid ${Color.secondary.main};
+    color: ${Color.secondary};
+    border: 1px solid ${Color.secondary};
     :hover {
-      background-color: ${Color.secondary.light};
-      border: 1px solid ${Color.secondary.light};
+      background-color: ${Color["secondary-light"]};
+      border: 1px solid ${Color["secondary-light"]};
     }
     :active {
-      background-color: ${Color.secondary.dark};
-      border: 1px solid ${Color.secondary.dark};
+      background-color: ${Color["secondary-dark"]};
+      border: 1px solid ${Color["secondary-dark"]};
     }
   `}
 
   ${({ danger }) =>
     danger &&
     `
-    color: ${Color.danger.main};
-    border: 1px solid ${Color.danger.main};
+    color: ${Color.red};
+    border: 1px solid ${Color.red};
     :hover {
-      background-color: ${Color.danger.light};
-      border: 1px solid ${Color.danger.light};
+      background-color: ${Color["red-dark"]};
+      border: 1px solid ${Color.red};
     }
     :active {
-      background-color: ${Color.danger.dark};
-      border: 1px solid ${Color.danger.dark};
+      background-color: ${Color["red-dark"]};
+      border: 1px solid ${Color["red-dark"]};
     }
   `}
 
   ${({ disabled }) =>
     disabled &&
     `
-    color: ${Color.black.A30} !important;
-    background-color: ${Color.black.A5} !important;
-    border: solid 1px ${Color.black.A5} !important;
+    color: ${Color["black-40"]} !important;
+    background-color: ${Color["black-10"]} !important;
+    border: solid 1px ${Color["black-10"]} !important;
     :hover {
-      background-color: ${Color.black.A5};
-      border: 1px solid ${Color.black.A5};
-      color: ${Color.black.A30};
+      background-color: ${Color["black-10"]};
+      border: 1px solid ${Color["black-10"]};
+      color: ${Color["black-40"]};
       cursor: not-allowed;
     }
   `}
@@ -211,16 +211,16 @@ export const OutlinedButton = styled.button`
     white &&
     `
       background-color: transparent;
-      border: 1px solid ${Color.white.main};
-      color: ${Color.white.main};
+      border: 1px solid ${Color["gray-20"]};
+      color: ${Color["gray-20"]};
       :hover {
-        background-color: ${Color.primary.light};
-        border: 1px solid ${Color.primary.light};
+        background-color: ${Color["primary-light"]};
+        border: 1px solid ${Color["primary-light"]};
       }
 
       :active {
-        background-color: ${Color.primary.dark};
-        border: 1px solid ${Color.primary.dark};
+        background-color: ${Color["primary-dark"]};
+        border: 1px solid ${Color["primary-dark"]};
       }
     `}
 `;
