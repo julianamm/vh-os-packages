cc1c666b68979635d845c66c411eecb2e98afd3c
Juliana Morais <julianamm88@gmail.com>
2020-02-10 13:25:52 -0800

    Fix test

--- packages/Components/VHTitleDescription/VHMainTitleDescription/.spec.js
+++ packages/Components/VHTitleDescription/VHMainTitleDescription/.spec.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import VHMainTitleDescription from '.';
 
-it('renders component without crashing', () => {
+it('renders VHMainTitleDescription component without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<VHMainTitleDescription title="Valgariam"
+    ReactDOM.render(<VHMainTitleDescription title="Drift Way"
+    rightTitle="(5 yr 9 mos)"
     description="E-comerce - Startup - Belo Horizonte - Brazil" />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
