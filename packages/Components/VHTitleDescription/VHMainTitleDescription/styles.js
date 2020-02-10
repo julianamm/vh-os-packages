5490c9890217cd868c7ba809da7fc200a4aa9a41
Juliana Morais <julianamm88@gmail.com>
2020-02-10 13:25:22 -0800

    Add styling

--- packages/Components/VHTitleDescription/VHMainTitleDescription/styles.js
+++ packages/Components/VHTitleDescription/VHMainTitleDescription/styles.js
@@ -4,18 +4,24 @@ import Color from '../../../Colors';
 export const Subtitle1 = styled.p`
     color: ${Color["primary-light"]};
     font-family: Roboto;
-    font-size: 18px;
-    font-weight: regular !important;
-    line-height: 22.4px !important;
-    display: block !important;
-    margin-right: 20px;
-    margin-top: 13px;
+    font-size: 22px;
+    font-weight: 500;
+    margin: 10px;
 `
 
 export const Platform1 = styled.p`
     color: ${Color["gray-60"]};
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: 500;
+    margin: 10px 0;
+    padding-top: 7px;
 `
 
 export const Caption = styled.p`
     color: ${Color["gray-90"]};
+    font-family: Roboto;
+    font-size: 12px;
+    font-weight: 500;
+    margin: 10px;
 `
