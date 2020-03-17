953594922f6423ccef0e257f2907473560dbc251
Jose Alves Durand Neto <durand.neto@gmail.com>
2020-03-17 14:50:52 -0700

    new version

--- dist/Templates/Announcement/styles.js
+++ dist/Templates/Announcement/styles.js
@@ -12,7 +12,7 @@ var _Colors = _interopRequireDefault(require("../../Colors"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n    height: 70vh;\n    background-color: ", ";\n    display: flex;\n    align-items: center;\n"]);
+  var data = _taggedTemplateLiteral(["\n    height: 70vh;\n    background-color: ", ";\n    display: flex;\n    align-items: center;\n    ", "\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -23,6 +23,9 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var Wrapper1 = _styledComponents.default.div(_templateObject(), _Colors.default["gradient-primary"]);
+var Wrapper1 = _styledComponents.default.div(_templateObject(), _Colors.default["gradient-primary"], function (_ref) {
+  var cover = _ref.cover;
+  return cover && "\n        background-image: url(".concat(cover, ");\n        background-repeat: no-repeat;\n        background-attachment: fixed;\n        background-size: cover;\n    ");
+});
 
 exports.Wrapper1 = Wrapper1;
