dd778259b83e29f9dde9e807d4e10e1643e64a30
Jose Alves Durand Neto <durand.neto@gmail.com>
2020-03-22 16:01:36 -0700

    adding preloading no sections

--- packages/Sections/Social/stories.js
+++ packages/Sections/Social/stories.js
@@ -45,4 +45,48 @@ storiesOf("Sections|Social", module)
                 }}
             />
         </div>
+    ))
+    .add("Preloading", () => (
+        <div style={{ backgroundColor: "#f2f2f2", height: "100vh", padding: "24px" }}>
+            <VHSkillsSection
+                preLoading
+                controls={{
+                    linkedin: {
+                        loading: true,
+                        error: false,
+                        success: false,
+                        message: 'Database error'
+                    },
+                    github: {
+                        loading: false,
+                        error: true,
+                        success: false,
+                        message: 'Database error'
+                    },
+                    world: {
+                        loading: false,
+                        error: false,
+                        success: false,
+                        message: 'Database error'
+                    },
+                }}
+
+                data={{
+                    linkedin: {
+                        value: '123',
+                    },
+                    github: {
+                        value: '123',
+                    },
+                    world: {
+                        value: '123',
+                    },
+
+                }}
+
+                onEvent={e => {
+                    console.log(e)
+                }}
+            />
+        </div>
     ));
