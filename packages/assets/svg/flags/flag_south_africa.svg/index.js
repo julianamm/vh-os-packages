075cb1a131b7aaa5e289e633b6d970e4d7738363
Juliana Morais <julianamm88@gmail.com>
2020-04-01 15:27:03 -0700

    Fix imgs svg bug

--- packages/assets/svg/flags/flag_south_africa.svg/index.js
+++ packages/assets/svg/flags/flag_south_africa.svg/index.js
@@ -4,14 +4,13 @@ import * as React from "react";
 const  flag_south_africa = props => (
     <svg xmlns="http://www.w3.org/2000/svg" width={props.width || "50"} height={props.height || "38"} fill={props.fill || "none"} viewBox={props.viewBox || "0 0 50 38"}>
         
-<g clipPath="url(#clip0)">
 <path fillRule="evenodd" clipRule="evenodd" d="M-0.000366481 29.8675V7.64632L16.5447 18.7569L-0.00769043 29.8675H-0.000366481Z" fill="black"/>
 <path fillRule="evenodd" clipRule="evenodd" d="M11.286 37.5065L29.8598 25.0117H56.2411V37.4992H11.286V37.5065Z" fill="#0000CC"/>
 <path fillRule="evenodd" clipRule="evenodd" d="M10.0995 -1.25233e-06H56.2411V12.5095H29.8525C29.8525 12.5095 10.3412 -0.12451 10.0995 -1.25233e-06Z" fill="#FF0000"/>
 <path fillRule="evenodd" clipRule="evenodd" d="M-0.000366481 4.68739V7.64632L16.5447 18.7496L-0.00769043 29.8675V32.8191L20.8293 18.7569L-0.00769043 4.68739H-0.000366481Z" fill="#FFCC00"/>
 <path fillRule="evenodd" clipRule="evenodd" d="M-0.000366211 4.6874V0H6.95749L29.0176 14.9411H56.2484V22.5728H29.0176L6.95017 37.4992H-0.000366211V32.8191L20.8293 18.7569L0.00695774 4.6874H-0.000366211Z" fill="#009933"/>
 <path fillRule="evenodd" clipRule="evenodd" d="M6.95016 0H11.286L29.8598 12.5022H56.2411V14.9411H29.0176L6.95016 0.00732406V0ZM6.95016 37.5065H11.286L29.8598 25.0117H56.2411V22.5801H29.0176L6.95016 37.4992V37.5065Z" fill="white"/>
-</g>
+
 <defs>
 <clipPath id="clip0">
 <path d="M0 0H50V37.5H0V0Z" fill="white"/>
