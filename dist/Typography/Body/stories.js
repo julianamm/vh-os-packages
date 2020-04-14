087da276b09124e8e35146268688e99dc3dcaf74
tiago <tiago@vanhack.com>
2020-04-14 16:35:00 -0700

    New version

--- dist/Typography/Body/stories.js
+++ dist/Typography/Body/stories.js
@@ -17,7 +17,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
     }
   }, /*#__PURE__*/_react.default.createElement(_.default, {
     xs: true,
-    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life. Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
+    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.\r Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
   }));
 }).add('sm', function () {
   return /*#__PURE__*/_react.default.createElement("div", {
@@ -26,7 +26,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
     }
   }, /*#__PURE__*/_react.default.createElement(_.default, {
     sm: true,
-    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life. Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
+    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.\r Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
   }));
 }).add('lg', function () {
   return /*#__PURE__*/_react.default.createElement("div", {
@@ -35,7 +35,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
     }
   }, /*#__PURE__*/_react.default.createElement(_.default, {
     lg: true,
-    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life. Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
+    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.\r Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
   }));
 });
 (0, _react2.storiesOf)('typography|Body/Reverse', module).add('xs', function () {
@@ -47,7 +47,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
   }, /*#__PURE__*/_react.default.createElement(_.default, {
     reverse: true,
     xs: true,
-    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life. Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
+    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.\r Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
   }));
 }).add('sm', function () {
   return /*#__PURE__*/_react.default.createElement("div", {
@@ -58,7 +58,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
   }, /*#__PURE__*/_react.default.createElement(_.default, {
     reverse: true,
     sm: true,
-    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life. Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
+    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.\r Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
   }));
 }).add('lg', function () {
   return /*#__PURE__*/_react.default.createElement("div", {
@@ -69,6 +69,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
   }, /*#__PURE__*/_react.default.createElement(_.default, {
     reverse: true,
     lg: true,
-    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life. Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
+    label: "body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.\r Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy."
   }));
 });
